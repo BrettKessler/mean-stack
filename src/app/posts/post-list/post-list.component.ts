cbfbf282431eb826b3406e90e693956d04bae5d7
Brett Kessler <brettkessler1@gmail.com>
2019-03-05 13:56:47 -0600

    major updates, add images, edit posts, delete posts

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component ({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post content'},
+  //   {title: 'Second Post', content: 'This is the second post content'},
+  //   {title: 'Third Post', content: 'This is the third post content'},
+  // ];
+  posts: Post [] = [];
+  isLoading = false;
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.postsService.getPosts();
+    this.isLoading = true;
+    this.postsSub = this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.isLoading = false;
+      this.posts = posts;
+    });
+  }
+
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
+
+
